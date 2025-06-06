79d9d6d935f811ba6357a5370899155df34d8fc5
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2025-06-06 18:10:18 +0200

    fix(canvaskit): avoid adding event listener for the stop button multiple times

--- js/canvaskit_demo.js
+++ js/canvaskit_demo.js
@@ -1,22 +1,21 @@
-// canvaskit_demo.js
-
-const WIDTH = 800;
-const HEIGHT = 600;
-const BOX_SIZE = 4;
+let isRunning = false;
 
 class BouncingRect {
-  constructor(canvasWidth, canvasHeight) {
-    this.box_x = canvasWidth / 2;
-    this.box_y = canvasHeight / 2;
+  constructor(canvasWidth, canvasHeight, boxSize) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.box_x = (canvasWidth - boxSize) * Math.random();
+    this.box_y = (canvasHeight - boxSize) * Math.random();
+    this.box_size = boxSize;
     this.velocity_x = getRandomVelocity();
     this.velocity_y = getRandomVelocity();
   }
 
   update() {
-    if (this.box_x <= 0 || this.box_x + BOX_SIZE >= WIDTH) {
+    if (this.box_x <= 0 || this.box_x + this.box_size >= this.canvasWidth) {
       this.velocity_x *= -1;
     }
-    if (this.box_y <= 0 || this.box_y + BOX_SIZE >= HEIGHT) {
+    if (this.box_y <= 0 || this.box_y + this.box_size >= this.canvasHeight) {
       this.velocity_y *= -1;
     }
     this.box_x += this.velocity_x;
@@ -27,8 +26,8 @@ class BouncingRect {
     const rect = CanvasKit.LTRBRect(
       this.box_x,
       this.box_y,
-      this.box_x + BOX_SIZE,
-      this.box_y + BOX_SIZE
+      this.box_x + this.box_size,
+      this.box_y + this.box_size
     );
     canvas.drawRect(rect, paint);
   }
@@ -44,13 +43,21 @@ function receiveResults(results, selector) {
   window.receiveResults(results, selector);
 }
 
-function run_canvaskit_benchmark() {
+function run_canvaskit_benchmark(
+  canvasWidth,
+  canvasHeight,
+  boxSize,
+  boxNumber
+) {
   CanvasKitInit({
     locateFile: (file) => "https://unpkg.com/canvaskit-wasm@0.19.0/bin/" + file,
   }).then((CanvasKit) => {
-    const canvas = document.getElementById("canvaskit");
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+    const canvasWrapEl = document.getElementById("window-wrap");
+    const canvasEl = document.createElement("canvas");
+    canvasEl.setAttribute("id", "canvaskit");
+    canvasEl.width = canvasWidth;
+    canvasEl.height = canvasHeight;
+    canvasWrapEl.append(canvasEl);
 
     const surface = CanvasKit.MakeCanvasSurface("canvaskit");
     const paint = new CanvasKit.Paint();
@@ -59,14 +66,13 @@ function run_canvaskit_benchmark() {
     paint.setAntiAlias(true);
 
     const rects = [];
-    for (let i = 0; i < 50000; i++) {
-      rects.push(new BouncingRect(WIDTH, HEIGHT));
+    for (let i = 0; i < boxNumber; i++) {
+      rects.push(new BouncingRect(canvasWidth, canvasHeight, boxSize));
     }
 
     const startTime = performance.now();
     let lastMeasureTime = startTime;
     let frameCount = 0;
-    const results = new Map();
 
     function drawFrame() {
       const canvasObj = surface.getCanvas();
@@ -81,30 +87,40 @@ function run_canvaskit_benchmark() {
       frameCount++;
 
       const now = performance.now();
-      if (now - lastMeasureTime > 3000) {
+      if (now - lastMeasureTime > 1000) {
         const elapsedSecs = (now - lastMeasureTime) / 1000.0;
-        const fps = frameCount / elapsedSecs;
-        results.set(rects.length, fps);
+        const fps = Math.round((frameCount / elapsedSecs) * 100) / 100;
         frameCount = 0;
         lastMeasureTime = now;
-        console.log("fps: ", fps);
 
-        // for (let i = 0; i < 10000; i++) {
-        //   rects.push(new BouncingRect(WIDTH, HEIGHT));
-        // }
+        const fpsEl = document.querySelector("#fps");
+        fpsEl.textContent = fps;
       }
-
-      // if (now - startTime > 10000) {
-      //     receiveResults(results, '#canvaskit-chart');
-      //     return;
-      // }
-
-      surface.requestAnimationFrame(drawFrame);
+      if (isRunning) {
+        console.log("redraw");
+        surface.requestAnimationFrame(drawFrame);
+      }
     }
-
+    isRunning = true;
     surface.requestAnimationFrame(drawFrame);
   });
 }
 
 const startButtonEl = document.querySelector("#start-canvaskit");
-startButtonEl.addEventListener("click", () => run_canvaskit_benchmark());
+startButtonEl.addEventListener("click", () => {
+  const canvasWidth = parseInt(document.querySelector("#canvas-width").value);
+  const canvasHeight = parseInt(document.querySelector("#canvas-height").value);
+  const boxSize = parseInt(document.querySelector("#box-size").value);
+  const boxNumber = parseInt(document.querySelector("#box-number").value);
+  run_canvaskit_benchmark(canvasWidth, canvasHeight, boxSize, boxNumber);
+});
+
+const stopButtonEl = document.querySelector("#stop");
+stopButtonEl.addEventListener("click", () => {
+  const canvaskitEl = document.querySelector("#canvaskit");
+  if (canvaskitEl) {
+    isRunning = false;
+    console.log("isRunning", isRunning);
+    canvaskitEl.remove();
+  }
+});
